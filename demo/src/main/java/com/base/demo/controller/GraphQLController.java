713e9b53c2f100b07d09cefcf6d567b016bc515d
juhy0987 <juhy0987@naver.com>
2025-02-21 08:22:21 +0900

    GraphQLController에 서비스 클래스 주입 추가

--- demo/src/main/java/com/base/demo/controller/GraphQLController.java
+++ demo/src/main/java/com/base/demo/controller/GraphQLController.java
@@ -1,13 +1,32 @@
 package com.base.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import com.base.demo.service.LocationService;
+import com.base.demo.service.PhotoService;
+import com.base.demo.service.ResortService;
+import com.base.demo.service.ReviewService;
+import com.base.demo.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class GraphQLController {
+  @Autowired
+  private UserService userService;
+  @Autowired
+  private ResortService resortService;
+  @Autowired
+  private LocationService locationService;
+  @Autowired
+  private ReviewService ReviewService;
+  @Autowired
+  private PhotoService photoService;
+
+  
   @QueryMapping
   public String hello() {
     return "Hello, GraphQL!";
