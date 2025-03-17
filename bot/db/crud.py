6f6718d22deaaf2184a7319d3dfa99644575694c
juhy0987 <juhy0987@naver.com>
2025-03-17 13:42:44 +0900

    트랜잭션 관리 기능 추가 및 데이터베이스 쿼리 함수 리팩토링

--- bot/db/crud.py
+++ bot/db/crud.py
@@ -6,51 +6,6 @@ from db.crud import *
 from db.schema import *
 
 
-def get_location(db: Session, location_id: int):
-  
-  return db.query(Location) \
-    .filter(Location.id == location_id) \
-    .first()
-
-def search_location(db: Session, searchSchema: LocationSearchSchema):
-  
-  query = db.query(Location)
-  if searchSchema.parent_id:
-    query = query.filter(Location.parent_id == searchSchema.parent_id)
-  if searchSchema.resort_id:
-    query = query.filter(Location.resort_id == searchSchema.resort_id)
-  if searchSchema.name:
-    query = query.filter(Location.name == searchSchema.name)
-  if searchSchema.alias:
-    query = query.filter(Location.alias == searchSchema.alias)
-  if searchSchema.coordinates:
-    query = query.filter(Location.coordinates == searchSchema.coordinates)
-  if searchSchema.timezone:
-    query = query.filter(Location.timezone == searchSchema.timezone)
-  
-  return query.all()
-
-def trace_route(db: Session):
-  
-  parent = None
-  while True:
-    cur = yield
-    if not cur:
-      break
-    
-    location = search_location(db, Location(parent_id=parent, name=cur))
-    if not location:
-      yield False
-    else:
-      location = location[0]
-    
-    parent = location.id
-    yield True
-  
-  yield parent
-
-
-
 class Transaction:
   def __init__(self, func):
     self.func = func
@@ -65,7 +20,64 @@ class Transaction:
     finally:
       db.commit()
       db.refresh(result)
+      db.close()
     return result
+  
+
+def get_location(db, location_id: int):
+  
+  return _get_location(db, location_id)
+
+def search_location(db, searchSchema: LocationSearchSchema):
+  
+  return _search_location(db, searchSchema)
+
+def trace_route(db):
+  
+  return _trace_route(db)
+
+
+def _get_location(db: Session, location_id: int):
+  
+  return db.query(Location) \
+    .filter(Location.id == location_id) \
+    .first()
+
+def _search_location(db: Session, searchSchema: LocationSearchSchema):
+  
+  query = db.query(Location)
+  if searchSchema.parent_id:
+    query = query.filter(Location.parent_id == searchSchema.parent_id)
+  if searchSchema.name:
+    query = query.filter(Location.name == searchSchema.name)
+  if searchSchema.alias:
+    query = query.filter(Location.alias == searchSchema.alias)
+  if searchSchema.coordinates:
+    query = query.filter(Location.coordinates == searchSchema.coordinates)
+  if searchSchema.timezone:
+    query = query.filter(Location.timezone == searchSchema.timezone)
+  
+  return query.all()
+
+def _trace_route(db: Session):
+  
+  parent = None
+  while True:
+    cur = yield
+    if not cur:
+      yield parent
+      continue
+        
+    
+    location = _search_location(db, Location(parent_id=parent, name=cur))
+    if not location:
+      yield False
+      continue
+    else:
+      location = location[0]
+    
+    parent = location.id
+    yield True
   
 
 def _create_location(db: Session, location: Location):
@@ -78,7 +90,7 @@ def _create_location(db: Session, location: Location):
     return target
   
   db.add(location)
-  return target
+  return location
 
 def _create_resort(db: Session, resort: Resort):
   
@@ -90,7 +102,7 @@ def _create_resort(db: Session, resort: Resort):
     return target
   
   db.add(resort)
-  return target
+  return resort
 
 
 @Transaction
