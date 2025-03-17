3393f215e9fd8855a7fdf2ddd9da7cfd545a6026
juhy0987 <juhy0987@naver.com>
2025-03-17 19:04:34 +0900

    GraphQLController에 리조트 조회 및 자동 완성 쿼리 추가, GraphQL 스키마 업데이트

--- demo/src/main/java/com/base/demo/controller/GraphQLController.java
+++ demo/src/main/java/com/base/demo/controller/GraphQLController.java
@@ -1,10 +1,15 @@
 package com.base.demo.controller;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-import com.base.demo.service.LocationService;
+import com.base.demo.dto.AutoCompleteDto;
+import com.base.demo.entity.Resort;
 import com.base.demo.service.PhotoService;
 import com.base.demo.service.ResortService;
 import com.base.demo.service.ReviewService;
@@ -20,15 +25,30 @@ public class GraphQLController {
   @Autowired
   private ResortService resortService;
   @Autowired
-  private LocationService locationService;
-  @Autowired
-  private ReviewService ReviewService;
+  private ReviewService reviewService;
   @Autowired
   private PhotoService photoService;
-
   
   @QueryMapping
   public String hello() {
     return "Hello, GraphQL!";
+  }
+
+  @QueryMapping
+  public Resort getResort(UUID id) {
+    Resort resort = resortService.getResort(id);
+    return resort;
+  }
+
+  // @QueryMapping
+  // public List<Resort> search() {
+  //   return resortService.getAll();
+  // }
+
+  @QueryMapping
+  public List<AutoCompleteDto> autoComplete(@Argument String query) {
+    List<AutoCompleteDto> tmp = resortService.autoComplete(query);
+    System.out.println(tmp);
+    return tmp;
   }
 }
