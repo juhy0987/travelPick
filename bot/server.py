08eec6059ea6537742b2b479d39c5558326d9231
juhy0987 <juhy0987@naver.com>
2025-04-16 23:24:03 +0900

    검색 기능 개선: 이미지 및 텍스트 검색 결과 통합, 검색 파라미터 추가 및 결과 정렬 로직 구현

--- bot/server.py
+++ bot/server.py
@@ -15,6 +15,19 @@ image_model_type="clip"
 image_collection_name = "image"
 image_cluster_collection_name = "image_cluster"
 
+search_parameter = {
+  0: 1.0,
+  1: 0.9,
+  2: 0.8,
+  3: 0.7,
+  4: 0.6,
+  5: 0.5,
+  6: 0.4,
+  7: 0.3,
+  8: 0.2,
+  9: 0.1,
+}
+
 text_store = chroma.VectorStore(os.getenv(text_model_type.upper()), text_model_type, text_collection_name)
 image_store = chroma.VectorStore(os.getenv(image_model_type.upper()), image_model_type, image_collection_name)
 text_cluster_store = chroma.VectorStore(None, None, text_cluster_collection_name)
@@ -32,11 +45,55 @@ def search():
     return jsonify({'error': 'No query provided'}), 400
   
   query = search_query.get('query')
+  cnt = search_query.get('cnt')
   dataurls = search_query.get('dataurls')
   images = [utils.parse_dataurl(dataurl) for dataurl in dataurls] if dataurls else []
   
   if query:
-    results = text_store.search(query, )
+    text_results = text_store.search(query, 
+                                count=int(cnt) if cnt else 10)
+    image_results = image_store.search(query,
+                                      count=int(cnt) if cnt else 10)
+  else:
+    text_results = {'ids': [], 'metadatas': [], 'documents': []}
+    image_results = image_store.search(images, 
+                                  count=int(cnt) if cnt else 10,
+                                  query_type="image")
+  
+  result = {}
+  for i, v in enumerate(zip(text_results['ids'], text_results['metadatas'], text_results['documents'])):
+    id, metadata, document = v
+    parent = metadata.get('parent')
+    if not parent:
+      continue
+      
+    if not result.get(parent):
+      result[parent] = {
+        "sum": 0.0,
+        "cnt": 0,
+      }
+      
+    result[parent]["sum"] += 0.5^(result[parent]["cnt"]+1) * search_parameter[i]
+    result[parent]["cnt"] += 1
+  
+  for i, v in enumerate(zip(image_results['ids'], image_results['metadatas'], image_results['documents'])):
+    id, metadata, document = v
+    parent = metadata.get('parent')
+    if not parent:
+      continue
+      
+    if not result.get(parent):
+      result[parent] = {
+        "sum": 0.0,
+        "cnt": 0,
+      }
+      
+    result[parent]["sum"] += 0.5^(result[parent]["cnt"]+1) * search_parameter[i]
+    result[parent]["cnt"] += 1
+  
+  result = sorted([{"id": key, "score": value["sum"]} for key, value in result.items()], key=lambda x: x[1], reverse=True)
+  return jsonify({"result": result}), 200
+      
 
 @app.route('/api/chroma', methods=['POST'])
 def store():
